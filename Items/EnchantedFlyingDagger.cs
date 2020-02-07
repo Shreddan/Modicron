686e826a2c72f90065b9fac55854da48458f872b
Shane Grehan <shanegrehan2005@gmail.com>
2020-02-07 22:53:04 +0000

    fixeded da fingz

--- Items/EnchantedFlyingDagger.cs
+++ Items/EnchantedFlyingDagger.cs
@@ -3,29 +3,31 @@ using Terraria.ModLoader;
 
 namespace Modicron.Items
 {
-    public class SuperSonicStabber : ModItem
+    public class EnchantedFlyingDagger : ModItem
     {
         public override void SetStaticDefaults()
         {
             // DisplayName.SetDefault("BasciSword"); // By default, capitalization in classnames will add spaces to the display name. You can customize the display name here by uncommenting this line.
-            Tooltip.SetDefault("Stabbin' around at the speed of sound!");
+            Tooltip.SetDefault("Throws a seemingly endless stream of daggers");
         }
 
         public override void SetDefaults()
         {
             item.damage = 25;
-            item.melee = true;
-            item.width = 50;
-            item.height = 50;
+            item.thrown = true;
+            item.width = 40;
+            item.height = 40;
             item.useTime = 2;
             item.useAnimation = 1;
-            item.useStyle = 3;
+            item.useStyle = 1;
             item.knockBack = 1;
             item.value = 1;
             item.rare = 3;
             item.UseSound = SoundID.Item1;
             item.autoReuse = true;
-            
+            item.noUseGraphic = true;
+            item.shoot = 93;
+            Items.shootSpeed = 15;
             
         }
 
--- Items/SuperSonicStabber.cs
+++ Items/SuperSonicStabber.cs
@@ -3,37 +3,41 @@ using Terraria.ModLoader;
 
 namespace Modicron.Items
 {
-	public class BasciSword : ModItem
-	{
-		public override void SetStaticDefaults() 
-		{
-			// DisplayName.SetDefault("BasciSword"); // By default, capitalization in classnames will add spaces to the display name. You can customize the display name here by uncommenting this line.
-			Tooltip.SetDefault("Your mum sucks testicles for money");
-		}
+    public class SuperSonicStabber : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            // DisplayName.SetDefault("BasciSword"); // By default, capitalization in classnames will add spaces to the display name. You can customize the display name here by uncommenting this line.
+            Tooltip.SetDefault("Stabbin' around at the speed of sound!");
+        }
 
-		public override void SetDefaults() 
-		{
-			item.damage = 10;
-			item.melee = true;
-			item.width = 40;
-			item.height = 40;
-			item.useTime = 20;
-			item.useAnimation = 20;
-			item.useStyle = 1;
-			item.knockBack = 42;
-			item.value = 10000;
-			item.rare = 2;
-			item.UseSound = SoundID.Item1;
-			item.autoReuse = true;
-		}
+        public override void SetDefaults()
+        {
+            item.damage = 25;
+            item.melee = true;
+            item.width = 50;
+            item.height = 50;
+            item.useTime = 2;
+            item.useAnimation = 1;
+            item.useStyle = 3;
+            item.knockBack = 1;
+            item.value = 1;
+            item.rare = 3;
+            item.UseSound = SoundID.Item1;
+            item.autoReuse = true;
 
-		public override void AddRecipes() 
-		{
-			ModRecipe recipe = new ModRecipe(mod);
-			recipe.AddIngredient(ItemID.DirtBlock, 3);
-			recipe.AddTile(TileID.WorkBenches);
-			recipe.SetResult(this);
-			recipe.AddRecipe();
-		}
-	}
+
+        }
+
+        public override void AddRecipes()
+        {
+            ModRecipe recipe = new ModRecipe(mod);
+            recipe.AddIngredient(ItemID.IronBar, 5);
+            recipe.AddIngredient(ItemID.Gel, 25);
+            recipe.AddIngredient(ItemID.FallenStar, 10);
+            recipe.AddTile(TileID.Anvils);
+            recipe.SetResult(this);
+            recipe.AddRecipe();
+        }
+    }
 }
