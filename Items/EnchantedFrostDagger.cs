555e089f12291cd694108532a947cc09958e3c2c
Shane Grehan <shanegrehan2005@gmail.com>
2020-02-07 22:39:27 +0000

    sss

--- Items/EnchantedFrostDagger.cs
+++ Items/EnchantedFrostDagger.cs
@@ -3,36 +3,38 @@ using Terraria.ModLoader;
 
 namespace Modicron.Items
 {
-    public class EnchantedFrostDagger : ModItem
+    public class SuperSonicStabber : ModItem
     {
         public override void SetStaticDefaults()
         {
             // DisplayName.SetDefault("BasciSword"); // By default, capitalization in classnames will add spaces to the display name. You can customize the display name here by uncommenting this line.
-            Tooltip.SetDefault("Throws a returning Frost Dagger that frost burns your foe");
+            Tooltip.SetDefault("Stabbin' around at the speed of sound!");
         }
 
         public override void SetDefaults()
         {
-            item.damage = 15;
+            item.damage = 25;
             item.melee = true;
-            item.width = 40;
-            item.height = 40;
-            item.useTime = 8;
+            item.width = 50;
+            item.height = 50;
+            item.useTime = 2;
             item.useAnimation = 1;
-            item.useStyle = 1;
-            item.knockBack = 3;
+            item.useStyle = 3;
+            item.knockBack = 1;
             item.value = 1;
             item.rare = 3;
             item.UseSound = SoundID.Item1;
             item.autoReuse = true;
-            item.UseNoGraphic = true;
+            
+            
         }
 
         public override void AddRecipes()
         {
             ModRecipe recipe = new ModRecipe(mod);
-            recipe.AddIngredient(ItemID.FallenStar, 5);
-            recipe.AddIngredient(ItemID.ThrowingKnife, 25);
+            recipe.AddIngredient(ItemID.IronBar, 5);
+            recipe.AddIngredient(ItemID.Gel, 25);
+            recipe.AddIngredient(ItemID.FallenStar, 10);
             recipe.AddTile(TileID.Anvils);
             recipe.SetResult(this);
             recipe.AddRecipe();
---
+++ Items/SuperSonicStabber.cs
@@ -0,0 +1,39 @@
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace Modicron.Items
+{
+	public class BasciSword : ModItem
+	{
+		public override void SetStaticDefaults() 
+		{
+			// DisplayName.SetDefault("BasciSword"); // By default, capitalization in classnames will add spaces to the display name. You can customize the display name here by uncommenting this line.
+			Tooltip.SetDefault("Your mum sucks testicles for money");
+		}
+
+		public override void SetDefaults() 
+		{
+			item.damage = 10;
+			item.melee = true;
+			item.width = 40;
+			item.height = 40;
+			item.useTime = 20;
+			item.useAnimation = 20;
+			item.useStyle = 1;
+			item.knockBack = 42;
+			item.value = 10000;
+			item.rare = 2;
+			item.UseSound = SoundID.Item1;
+			item.autoReuse = true;
+		}
+
+		public override void AddRecipes() 
+		{
+			ModRecipe recipe = new ModRecipe(mod);
+			recipe.AddIngredient(ItemID.DirtBlock, 3);
+			recipe.AddTile(TileID.WorkBenches);
+			recipe.SetResult(this);
+			recipe.AddRecipe();
+		}
+	}
+}
